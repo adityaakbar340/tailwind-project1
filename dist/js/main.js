a1230b42a33e6352987c411e75a26a2011d6f483
akbarullah99 <adityaakbar340@gmail.com>
2023-06-27 09:28:01 +0700

    tambah library swiper

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,12 @@
 const hamburgerButton = document.querySelector('#hamburger-button');
 const navMenu = document.getElementById('nav-menu');
 
-
-
 hamburgerButton.addEventListener('click', function () {
   navMenu.classList.toggle('block');
   navMenu.classList.toggle('hidden');
   hamburgerButton.classList.toggle("hamburger-active");
 });
+
 
 // window.addEventListener("DOMContentLoaded", (event) => {
 //   const el = document.getElementById('hamburger-button');
