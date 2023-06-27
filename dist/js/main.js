3931d2d0f143f86c05f150b7587a76650d935db1
akbarullah99 <adityaakbar340@gmail.com>
2023-06-27 07:49:44 +0700

    header dan carousel

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+const hamburgerButton = document.querySelector('#hamburger-button');
+const navMenu = document.getElementById('nav-menu');
+
+
+
+hamburgerButton.addEventListener('click', function () {
+  navMenu.classList.toggle('block');
+  navMenu.classList.toggle('hidden');
+  hamburgerButton.classList.toggle("hamburger-active");
+});
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   const el = document.getElementById('hamburger-button');
+//   if (el) {
+//     el.addEventListener('click', () => alert('aktif'));
+//   }
+// });
